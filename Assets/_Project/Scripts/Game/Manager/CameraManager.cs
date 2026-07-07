473b3e7c20415cce0d3389d29c0ce8758105112c
nkcsimizu <nkc25.simizu.kouki@gmail.com>
2026-07-07 18:39:23 +0900

    Add.サンダーを落とせるようにしました。 　　カメラ機能を追加しました。

---
+++ Assets/_Project/Scripts/Game/Manager/CameraManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+using Unity.Cinemachine.Editor;
+using Unity.Cinemachine.TargetTracking;
+using Rina_Script;
+
+namespace Simizu
+{
+    public enum CameraPos
+    {
+        LeftUpper,      // 左上
+        LeftLower,      // 左下
+        RightUpper,     // 右上
+        RightLower,     // 右下
+        Player,         // プレイヤー
+    }
+
+
+    public class CameraManager : MonoBehaviour
+    {
+        public static CameraManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if(Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        // プレイヤーを追従するカメラ
+        [SerializeField] public CinemachineCamera pCamera;
+
+        // 他のカメラ
+        [SerializeField] public CinemachineCamera[] nCameras;
+
+        private CinemachineCamera currentCamera;
+
+        // 非選択時のバーチャルカメラの優先度
+        [SerializeField] private int unselectedPriority = 0;
+
+        // 選択時のバーチャルカメラの優先度
+        [SerializeField] private int selectedPriority = 10;
+
+        private void Start()
+        {
+            currentCamera = pCamera;
+            ResetCamera();
+        }
+
+        public void ChangeCamera(CameraPos cameraPos)
+        {
+            currentCamera.Priority = unselectedPriority;
+            nCameras[(int)cameraPos].Priority = selectedPriority;
+        }
+
+        public void ResetCamera()
+        {
+            for(int i = 0;  i < nCameras.Length; i++)
+            {
+                nCameras[i].Priority = unselectedPriority;
+            }
+            currentCamera.Priority = selectedPriority;
+        }
+    }
+}
